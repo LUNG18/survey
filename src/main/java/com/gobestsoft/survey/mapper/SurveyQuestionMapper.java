a48b14863e19590c7e04021760a55f99bc36aaeb
LUNG18 <466279917@qq.com>
2019-08-05 12:00:59 +0800

    删除未使用的方法 修改端口

--- src/main/java/com/gobestsoft/survey/mapper/SurveyQuestionMapper.java
+++ src/main/java/com/gobestsoft/survey/mapper/SurveyQuestionMapper.java
@@ -2,19 +2,10 @@ package com.gobestsoft.survey.mapper;
 
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.gobestsoft.survey.pojo.SurveyQuestionPojo;
-import org.apache.ibatis.annotations.Param;
-
-import java.util.List;
 
 /**
  * 问卷题目
  */
 public interface SurveyQuestionMapper extends BaseMapper<SurveyQuestionPojo> {
 
-
-    /**
-     * 查询题号
-     * @return
-     */
-    List<Integer> selectPosition(@Param("surveyId") Integer surveyId, @Param("answer") Integer answer);
 }
